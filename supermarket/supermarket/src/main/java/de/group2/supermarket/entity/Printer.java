63b1d86307853ad5b651175fa35362382b5ecdc8
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-10-04 10:41:44 +0200

    U

--- supermarket/supermarket/src/main/java/de/group2/supermarket/entity/Printer.java
+++ supermarket/supermarket/src/main/java/de/group2/supermarket/entity/Printer.java
@@ -3,11 +3,41 @@ package de.group2.supermarket.entity;
 import de.group2.supermarket.config.UuidIdentifiedEntity;
 
 public class Printer extends UuidIdentifiedEntity{
-    
+
     private String type;
 
     private String name;
-    
+
     private int nrOfDesk;
+
+    public Printer(String type, String name, int nrOfDesk) {
+        this.type = type;
+        this.name = name;
+        this.nrOfDesk = nrOfDesk;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNrOfDesk() {
+        return nrOfDesk;
+    }
+
+    public void setNrOfDesk(int nrOfDesk) {
+        this.nrOfDesk = nrOfDesk;
+    }
     
 }
