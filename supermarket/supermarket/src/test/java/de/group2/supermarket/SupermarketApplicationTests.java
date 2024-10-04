24b6e7266c0b14040c883635e166552dfb27db03
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-10-04 09:27:07 +0200

    Spring Boot

---
+++ supermarket/supermarket/src/test/java/de/group2/supermarket/SupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package de.group2.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
