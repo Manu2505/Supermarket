ca1cc1dda4cd465e4e3d33d1e15cca927cd2e6b8
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-10-04 09:41:34 +0200

    Update

---
+++ supermarket/supermarket/src/main/java/de/group2/supermarket/config/SimpleMongoConfig.java
@@ -0,0 +1,34 @@
+package de.group2.supermarket.config;
+
+import org.bson.UuidRepresentation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "de.mathisneunzig.facilitymanagement.fm.repo", repositoryBaseClass = CustomMongoRepositoryImpl.class)
+public class SimpleMongoConfig {
+ 
+    @Bean
+    public MongoClient mongo() {
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://admin:MongoDBAdmin@mathisneunzig.gxcl7jz.mongodb.net/?retryWrites=true&w=majority");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+          .uuidRepresentation(UuidRepresentation.STANDARD)
+          .applyConnectionString(connectionString)
+          .build();
+        
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "test");
+    }
+    
+}
---
+++ supermarket/supermarket/src/main/java/de/group2/supermarket/config/UuIdIdentifiedEntityEventListener.java
@@ -0,0 +1,21 @@
+package de.group2.supermarket.config;
+
+import java.util.UUID;
+
+import org.springframework.data.mongodb.core.mapping.event.AbstractMongoEventListener;
+import org.springframework.data.mongodb.core.mapping.event.BeforeConvertEvent;
+
+public class UuidIdentifiedEntityEventListener extends AbstractMongoEventListener<UuidIdentifiedEntity> {
+    
+    @Override
+    public void onBeforeConvert(BeforeConvertEvent<UuidIdentifiedEntity> event) {
+        
+        super.onBeforeConvert(event);
+        UuidIdentifiedEntity entity = event.getSource();
+        
+        if(entity.getId() == null) {
+            entity.setId(UUID.randomUUID());
+        } 
+    }
+    
+}
