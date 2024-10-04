5f9966ab18ebea786c8cb8d967b25f58d47d8723
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-10-04 10:10:37 +0200

    U

--- supermarket/supermarket/src/main/java/de/group2/supermarket/entity/Printer.java
+++ supermarket/supermarket/src/main/java/de/group2/supermarket/entity/Printer.java
@@ -4,5 +4,8 @@ import de.group2.supermarket.config.UuidIdentifiedEntity;
 
 public class Printer extends UuidIdentifiedEntity{
     
+    private String name;
+    
+    private int nrOfDesk;
     
 }
