e6a973ca675c2c200f44d879afc4a3d2198bcc1c
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-10-26 20:24:29 +0200

    added wbconfig because of cors

---
+++ supermarket/supermarket/src/main/java/de/group2/supermarket/config/WebConfig.java
@@ -0,0 +1,17 @@
+package de.group2.supermarket.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+     public class WebConfig implements WebMvcConfigurer {
+
+         @Override
+         public void addCorsMappings(CorsRegistry registry) {
+             registry.addMapping("/**") // Allow CORS for all paths
+                     .allowedOrigins("http://localhost:5173")
+                     .allowedMethods("GET", "POST", "PUT", "DELETE")
+                     .allowedHeaders("*");
+         }
+     }
