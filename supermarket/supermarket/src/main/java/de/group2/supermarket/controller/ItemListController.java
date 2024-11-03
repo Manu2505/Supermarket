9ad343fe8b78dae059b80c8b7a629f9072204681
Manu2505 <148750849+Manu2505@users.noreply.github.com>
2024-11-03 14:25:47 +0100

    U

--- supermarket/supermarket/src/main/java/de/group2/supermarket/controller/ItemListController.java
+++ supermarket/supermarket/src/main/java/de/group2/supermarket/controller/ItemListController.java
@@ -47,11 +47,10 @@ public class ItemListController {
     }
  
     
-    //auch da keine Ahnung ob das für Update passt
+
     @PutMapping("/{id}") // localhost:8080/itemList/"some id"
     public ResponseEntity<Object> update(@PathVariable UUID id, @RequestBody ItemList itemList){
         try{
-            itemList.setId(id);
             return new ResponseEntity<Object>(itemListRepository.save(itemList), HttpStatus.OK);
         }catch (NoSuchElementException e){
             return new ResponseEntity<Object>("ItemList with id " + id + " could not be found", HttpStatus.NOT_FOUND);
